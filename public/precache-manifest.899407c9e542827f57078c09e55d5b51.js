f48f66d15b911efe27721418db807cf555d6a3eb
Do <Tuyen.Do@coyotes.usd.edu>
2018-12-31 15:47:59 -0600

    First commit

---
+++ public/precache-manifest.899407c9e542827f57078c09e55d5b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4243060d7b24a2bc4f57ce8348a5b6c5",
+    "url": "/static/media/logo.4243060d.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e4f9c73239fc6bdb444f",
+    "url": "/static/js/main.e4f9c732.chunk.js"
+  },
+  {
+    "revision": "74448b3e6e951fad4308",
+    "url": "/static/js/1.74448b3e.chunk.js"
+  },
+  {
+    "revision": "e4f9c73239fc6bdb444f",
+    "url": "/static/css/main.1a6a57e2.chunk.css"
+  },
+  {
+    "revision": "953529999348c400f3004820aaa887f1",
+    "url": "/index.html"
+  }
+];
